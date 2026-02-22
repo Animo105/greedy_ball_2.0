071cfd15df9a7d61c042c6ac09a5cd50ae93c644
Animo105 <emeric2005leclerc@gmail.com>
2026-02-21 19:55:43 -0500

    Ball suposément fonctionnel

--- Balls/Scripts/abstract_ball.gd
+++ Balls/Scripts/abstract_ball.gd
@@ -1,5 +1,12 @@
 @abstract extends RigidBody2D
 class_name Ball
 
+func _init() -> void:
+	body_entered.connect(_collision_on_module)
+
+func _collision_on_module(body : Node):
+	if body is Module:
+		body.collision(self)
+
 @export var value : int = 0
 @export var speed : float = 1.0
