ccd4a83854a34b98a79c3b9552d78927a7b20527
Animo105 <emeric2005leclerc@gmail.com>
2026-02-21 19:30:55 -0500

    démo resource module

---
+++ Modules/Resources/module_res.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name ResourceModule
+
+@export var module_name : String
+@export var basic_price : float
+@export var icon_texture : Texture2D
+@export var module_scene : PackedScene
