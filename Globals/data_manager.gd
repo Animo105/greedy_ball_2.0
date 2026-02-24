64d4d8cb4807931a6dc4f5e4cac577c81e46cff8
Animo105 <emeric2005leclerc@gmail.com>
2026-02-23 19:47:26 -0500

    WIP: DataManager

--- Globals/data_manager.gd
+++ Globals/data_manager.gd
@@ -1,4 +1,19 @@
 extends Node
 
-var loaded_balls : Array[Ball]
+const MODULES_RESOURCE_FOLDER_PATH : String = "res://Balls/Resources/"
+const BALLS_RESOURCE_FOLDER_PATH : String = "res://Modules/Resources/"
+
+var loaded_balls : Array[ResourceBall]
 var loaded_resource_modules : Array[ResourceModule]
+
+func _ready() -> void:
+	# Check before anything, crash if it doesn't work
+	assert(DirAccess.dir_exists_absolute(MODULES_RESOURCE_FOLDER_PATH) && DirAccess.dir_exists_absolute(BALLS_RESOURCE_FOLDER_PATH))
+	
+	# Load Modules
+	for file_name in DirAccess.get_files_at(MODULES_RESOURCE_FOLDER_PATH):
+		print(file_name)
+	
+	# Load Balls
+	
+	pass
