c4c8bdee682c31bc4ff53b5075d9e284e0b63875
Animo105 <emeric2005leclerc@gmail.com>
2026-02-26 11:22:51 -0500

    WIP: building

--- Globals/game_manager.gd
+++ Globals/game_manager.gd
@@ -16,8 +16,23 @@ var game_state : GameStates = GameStates.NONE:
 		game_state = value
 		game_state_changed.emit(value)
 
-var starting_ball_positon : Vector2 = Vector2.ZERO
+func _process(_delta: float) -> void:
+	match game_state:
+		GameStates.BUILDING:
+			if not module_in_building:
+				return
+			module_in_building.global_position = get_viewport().get_mouse_position()
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton && event.is_pressed():
+		if event.index_button == 1:
+			if game_state == GameStates.BUILDING:
+				pass
+			elif game_state == GameStates.REMOVING:
+				pass
+
+# PLAYING VARIABLES AND FUNCTIONS
+var starting_ball_positon : Vector2 = Vector2.ZERO
 var balls : Array[Ball]
 var ball_type : String = ""
 
@@ -57,3 +72,16 @@ func end_round() -> void:
 		balls = []
 		game_state = GameStates.NONE
 		round_ended.emit()
+
+
+# BUILDING VARIABLES AND FUNCTIONS
+var modules : Array[Module]
+var module_in_building : Module = null
+
+func build_module(module : Module, price : float):
+	if game_state != GameStates.NONE :
+		return
+	module_in_building = module
+	get_tree().current_scene.add_child(module_in_building)
+	game_state = GameStates.BUILDING
+	
